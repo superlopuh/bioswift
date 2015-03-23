c1da9522a585d651ef40bbd83d3c505e53dbd5f8
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-03-23 20:39:02 +0000

    copied Nucleotide to new core

---
+++ BioSwiftCore/BioSwiftCore/Nucleotide.swift
@@ -0,0 +1,35 @@
+//
+//  Nucleotide.swift
+//  BioSwiftCore
+//
+//  Created by Alexandre Lopoukhine on 23/03/2015.
+//  Copyright (c) 2015 bluetatami. All rights reserved.
+//
+
+import Foundation
+
+public enum Nucleotide: Character {
+    case A = "A"
+    case C = "C"
+    case G = "G"
+    case T = "T"
+    
+    public init?(char: Character) {
+        switch char {
+        case Character("A"):
+            self = .A
+        case Character("C"):
+            self = .C
+        case Character("G"):
+            self = .G
+        case Character("T"):
+            self = .T
+        default:
+            return nil
+        }
+    }
+    
+    public var description: String {
+        return String(self.rawValue)
+    }
+}

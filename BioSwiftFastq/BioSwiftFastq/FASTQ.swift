8406c39bbfbcde93da179af933e1baaba506b368
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-03-23 20:45:15 +0000

    added FASTQ and FASTQType files to new FASTQ

---
+++ BioSwiftFastq/BioSwiftFastq/FASTQ.swift
@@ -0,0 +1,20 @@
+//
+//  FASTQ.swift
+//  BioSwiftFastq
+//
+//  Created by Alexandre Lopoukhine on 23/03/2015.
+//  Copyright (c) 2015 bluetatami. All rights reserved.
+//
+
+import Foundation
+import BioSwiftCore
+
+public struct FASTQ {
+    // Original Info
+    let fastqInfoString: String
+    let dnaString: String
+    let qualityString: String
+    
+    // Derived structure
+    //    let probDNASequence: ProbDNASequence
+}
---
+++ BioSwiftFastq/BioSwiftFastq/FASTQType.swift
@@ -0,0 +1,13 @@
+//
+//  FASTQType.swift
+//  BioSwiftFastq
+//
+//  Created by Alexandre Lopoukhine on 23/03/2015.
+//  Copyright (c) 2015 bluetatami. All rights reserved.
+//
+
+import Foundation
+
+enum FASTQType {
+    case Illumina18, Illumina15, Sanger
+}

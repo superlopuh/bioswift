6ecbdfbc974b615686b3244ec6f2f4c914e20672
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-03-23 17:00:29 +0000

    added Nucleotide enum

--- bioswift-core/bioswift-core/Nucleotide.swift
+++ bioswift-core/bioswift-core/Nucleotide.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+
+public enum Nucleotide: Character {
+    case A = "A"
+    case C = "C"
+    case G = "G"
+    case T = "T"
+    
+    public init?(char: Character) {
+        switch char {
+        case Character("A"):
+            self = .A
+        case Character("C"):
+            self = .C
+        case Character("G"):
+            self = .G
+        case Character("T"):
+            self = .T
+        default:
+            return nil
+        }
+    }
+    
+    public var description: String {
+        return String(self.rawValue)
+    }
+}

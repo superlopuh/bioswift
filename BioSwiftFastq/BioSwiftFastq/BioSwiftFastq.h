64753fc49e1988089e7758a06560632db11cc846
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-03-23 20:42:22 +0000

    created BioSwiftFastq project

---
+++ BioSwiftFastq/BioSwiftFastq/BioSwiftFastq.h
@@ -0,0 +1,19 @@
+//
+//  BioSwiftFastq.h
+//  BioSwiftFastq
+//
+//  Created by Alexandre Lopoukhine on 23/03/2015.
+//  Copyright (c) 2015 bluetatami. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BioSwiftFastq.
+FOUNDATION_EXPORT double BioSwiftFastqVersionNumber;
+
+//! Project version string for BioSwiftFastq.
+FOUNDATION_EXPORT const unsigned char BioSwiftFastqVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BioSwiftFastq/PublicHeader.h>
+
+

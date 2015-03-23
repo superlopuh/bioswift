94e033f32418c417a1ae3d4ca73a237ae1755bdf
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-03-23 20:29:58 +0000

    added FASTQ file

---
+++ bioswift-fastq/bioswift-fastq/FASTQ.swift
@@ -0,0 +1,20 @@
+//
+//  FASTQ.swift
+//  bioswift-fastq
+//
+//  Created by Alexandre Lopoukhine on 23/03/2015.
+//  Copyright (c) 2015 bluetatami. All rights reserved.
+//
+
+import Foundation
+
+public struct FASTQ {
+    // Original Info
+    let fastqInfoString: String
+    let dnaString: String
+    let qualityString: String
+    
+    // Derived structure
+//    let probDNASequence: ProbDNASequence
+}
+

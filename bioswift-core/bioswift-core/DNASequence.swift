88986b9c8f65bf06229fafd2d2eff25f09d1a4b7
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-03-23 20:05:30 +0000

    added DNASequence file

---
+++ bioswift-core/bioswift-core/DNASequence.swift
@@ -0,0 +1,10 @@
+//
+//  DNASequence.swift
+//  bioswift-core
+//
+//  Created by Alexandre Lopoukhine on 23/03/2015.
+//  Copyright (c) 2015 bluetatami. All rights reserved.
+//
+
+import Foundation
+

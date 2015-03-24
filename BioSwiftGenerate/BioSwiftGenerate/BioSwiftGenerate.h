52ab9e08c1f3305d58c5b347016af5d2a02c40f9
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-03-24 09:49:07 +0000

    created BioSwiftGenerate framework and added it to workspace

---
+++ BioSwiftGenerate/BioSwiftGenerate/BioSwiftGenerate.h
@@ -0,0 +1,19 @@
+//
+//  BioSwiftGenerate.h
+//  BioSwiftGenerate
+//
+//  Created by Alexandre Lopoukhine on 24/03/2015.
+//  Copyright (c) 2015 bluetatami. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BioSwiftGenerate.
+FOUNDATION_EXPORT double BioSwiftGenerateVersionNumber;
+
+//! Project version string for BioSwiftGenerate.
+FOUNDATION_EXPORT const unsigned char BioSwiftGenerateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BioSwiftGenerate/PublicHeader.h>
+
+

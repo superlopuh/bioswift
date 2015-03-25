b89943adf1e94c510193ee97e0aa039cfe7e5fe3
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-03-25 11:14:04 +0000

    added initialiser from strings

--- BioSwiftFastq/BioSwiftFastq/FASTQ.swift
+++ BioSwiftFastq/BioSwiftFastq/FASTQ.swift
@@ -10,11 +10,36 @@ import Foundation
 import BioSwiftCore
 
 public struct FASTQ {
+    let fastqType: FASTQType
+    
     // Original Info
     let fastqInfoString: String
     let dnaString: String
     let qualityString: String
     
     // Derived structure
-    //    let probDNASequence: ProbDNASequence
+    let probDNASequence: ProbDNASequence
+    
+    public init?(fastqInfoString: String, dnaString: String, qualityString: String, fastqType: FASTQType) {
+        self.fastqType          = fastqType
+        
+        self.fastqInfoString    = fastqInfoString
+        self.dnaString          = dnaString
+        self.qualityString      = qualityString
+        
+        // Convert qualityString to probability array
+        let optionalErrorProbArray = Array(qualityString).map() {
+            return fastqType.charToProb($0)
+        }
+        
+        if let errorProbArray = unwrap(optionalErrorProbArray) {
+            if let probDNASequence = ProbDNASequence(dnaString: dnaString, errorProbArray: errorProbArray) {
+                self.probDNASequence = probDNASequence
+            } else {
+                return nil
+            }
+        } else {
+            return nil
+        }
+    }
 }

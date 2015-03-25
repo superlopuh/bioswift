2a6b7bda5df9a58fed6aace7e6158cc9ecba767a
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-03-25 10:44:03 +0000

    added ProbNucleotide struct

---
+++ BioSwiftCore/BioSwiftCore/ProbNucleotide.swift
@@ -0,0 +1,14 @@
+//
+//  ProbNucleotide.swift
+//  BioSwiftCore
+//
+//  Created by Alexandre Lopoukhine on 24/03/2015.
+//  Copyright (c) 2015 bluetatami. All rights reserved.
+//
+
+import Foundation
+
+struct ProbNucleotide {
+    let nucleotide: Nucleotide
+    let errorProb: Double
+}

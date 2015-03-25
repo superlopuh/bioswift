ea734b8cd80b5e4d186f18d1e764a3efa36e3e5e
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-03-25 16:02:33 +0000

    added functions to extract FASTQ sequences from a file

--- BioSwiftFastq/BioSwiftFastq/FASTQReader.swift
+++ BioSwiftFastq/BioSwiftFastq/FASTQReader.swift
@@ -7,3 +7,61 @@
 //
 
 import Foundation
+
+public class FASTQReader {
+    // Get as many FASTQ sequences from start of file as possible
+    // Does not check for file being valid
+    public static func getArrayFromFile(fileAddress: NSURL, ofFASTQType fastqType: FASTQType) -> [FASTQ] {
+        
+        var sequences = [FASTQ]()
+        
+        if let fileContent = NSString(contentsOfURL: fileAddress, encoding: NSUTF8StringEncoding, error: nil) {
+            println(fileContent.length)
+            let fastqStringArray = fileContent.componentsSeparatedByCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()) as! [String]
+            
+            var gen = fastqStringArray.generate()
+            while
+                let infoString    = gen.next(),
+                let dnaString     = gen.next(),
+                let plusString    = gen.next(),
+                let qualityString = gen.next(){
+                    if let fastq = FASTQ(fastqInfoString: infoString, dnaString: dnaString, qualityString: qualityString, fastqType: fastqType) {
+                        sequences.append(fastq)
+                    } else {
+                        break
+                    }
+            }
+        }
+        // End of file
+        
+        return sequences
+    }
+    
+    public static func getArrayFromFile(fileAddress: NSURL, ofFASTQType fastqType: FASTQType, @noescape usingFilter filter: FASTQ -> Bool) -> [FASTQ] {
+        
+        var sequences = [FASTQ]()
+        
+        if let fileContent = NSString(contentsOfURL: fileAddress, encoding: NSUTF8StringEncoding, error: nil) {
+            println(fileContent.length)
+            let fastqStringArray = fileContent.componentsSeparatedByCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()) as! [String]
+            
+            var gen = fastqStringArray.generate()
+            while
+                let infoString    = gen.next(),
+                let dnaString     = gen.next(),
+                let plusString    = gen.next(),
+                let qualityString = gen.next(){
+                    if let fastq = FASTQ(fastqInfoString: infoString, dnaString: dnaString, qualityString: qualityString, fastqType: fastqType) {
+                        if filter(fastq) {
+                            sequences.append(fastq)
+                        }
+                    } else {
+                        break
+                    }
+            }
+        }
+        // End of file
+        
+        return sequences
+    }
+}

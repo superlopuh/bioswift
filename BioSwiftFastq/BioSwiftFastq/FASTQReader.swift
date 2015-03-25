df213cff22591233605939ac511c1fb6df957c1d
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-03-25 12:06:58 +0000

    created FASTQReader file

---
+++ BioSwiftFastq/BioSwiftFastq/FASTQReader.swift
@@ -0,0 +1,9 @@
+//
+//  FASTQReader.swift
+//  BioSwiftFastq
+//
+//  Created by Alexandre Lopoukhine on 25/03/2015.
+//  Copyright (c) 2015 bluetatami. All rights reserved.
+//
+
+import Foundation

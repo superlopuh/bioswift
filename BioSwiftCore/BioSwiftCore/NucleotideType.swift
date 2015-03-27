cb738e16017cfd9724bfd9fae24a7037c8def4f2
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-03-27 18:35:46 +0000

    empty protocol NucleotideType

---
+++ BioSwiftCore/BioSwiftCore/NucleotideType.swift
@@ -0,0 +1,12 @@
+//
+//  NucleotideType.swift
+//  BioSwiftCore
+//
+//  Created by Alexandre Lopoukhine on 27/03/2015.
+//  Copyright (c) 2015 bluetatami. All rights reserved.
+//
+
+import Foundation
+
+public protocol NucleotideType {
+}

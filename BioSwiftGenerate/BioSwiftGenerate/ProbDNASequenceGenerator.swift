e11906b040e9a93d932a3900eb0fead339d48c9f
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-03-27 15:26:01 +0000

    created basic ProbDNASequence generator

--- BioSwiftGenerate/BioSwiftGenerate/ProbDNASequenceGenerator.swift
+++ BioSwiftGenerate/BioSwiftGenerate/ProbDNASequenceGenerator.swift
@@ -7,8 +7,38 @@
 //
 
 import Foundation
+import BioSwiftCore
 
 public class ProbDNASequenceGenerator {
+    public static func generateSequence(ofLength length: Int) -> ProbDNASequence {
+        var probNucleotideArray = [ProbNucleotide]()
+        
+        for i in 0..<length {
+            let randomNumber = arc4random_uniform(4)
+            
+            let nucleotide: Nucleotide
+            switch randomNumber {
+            case 0:
+                nucleotide = .A
+            case 1:
+                nucleotide = .C
+            case 2:
+                nucleotide = .G
+            case 3:
+                nucleotide = .T
+            default:
+                assertionFailure("Random number generator generated number out of 0..<4 range")
+            }
+            
+            let errorProb = generateErrorProb()
+            
+            probNucleotideArray.append(ProbNucleotide.Known(nucleotide, errorProb))
+        }
+        
+        assert(probNucleotideArray.count == length, "NucleotideArray length doesn't match up")
+        
+        return ProbDNASequence(probNucleotideArray)
+    }
     
     public static func generateErrorProb() -> Double {
         return 0.5

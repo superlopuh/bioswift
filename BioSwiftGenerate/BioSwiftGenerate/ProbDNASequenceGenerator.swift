03b7d8d7eb4a1d7d95f58b294824503f8fb8c287
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-03-27 15:04:47 +0000

    created ProbDNASequenceGenerator class

---
+++ BioSwiftGenerate/BioSwiftGenerate/ProbDNASequenceGenerator.swift
@@ -0,0 +1,13 @@
+//
+//  ProbDNASequenceGenerator.swift
+//  BioSwiftGenerate
+//
+//  Created by Alexandre Lopoukhine on 27/03/2015.
+//  Copyright (c) 2015 bluetatami. All rights reserved.
+//
+
+import Foundation
+
+public class ProbDNASequenceGenerator {
+    
+}

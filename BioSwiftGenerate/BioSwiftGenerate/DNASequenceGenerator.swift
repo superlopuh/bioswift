9d61c2f637e0ce740062c0a53eabd07f12d153e5
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-03-27 15:03:07 +0000

    added generation of random DNASequence

--- BioSwiftGenerate/BioSwiftGenerate/DNASequenceGenerator.swift
+++ BioSwiftGenerate/BioSwiftGenerate/DNASequenceGenerator.swift
@@ -7,7 +7,34 @@
 //
 
 import Foundation
+import BioSwiftCore
 
 public class DNASequenceGenerator {
-    
+    public static func generateSequence(ofLength length: Int) -> DNASequence {
+        var nucleotideArray = [Nucleotide]()
+        
+        for i in 0..<length {
+            let randomNumber = arc4random_uniform(4)
+            
+            let nucleotide: Nucleotide
+            switch randomNumber {
+            case 0:
+                nucleotide = .A
+            case 1:
+                nucleotide = .C
+            case 2:
+                nucleotide = .G
+            case 3:
+                nucleotide = .T
+            default:
+                assertionFailure("Random number generator generated number out of 0..<4 range")
+            }
+            
+            nucleotideArray.append(nucleotide)
+        }
+        
+        assert(nucleotideArray.count == length, "NucleotideArray length doesn't match up")
+        
+        return DNASequence(nucleotideSlice: nucleotideArray)
+    }
 }

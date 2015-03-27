6a54f491bdfcebc0d5efa691c289c23aca0dab07
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-03-27 13:05:56 +0000

    added DNASequenceGenerator class to Generate

---
+++ BioSwiftGenerate/BioSwiftGenerate/DNASequenceGenerator.swift
@@ -0,0 +1,13 @@
+//
+//  DNASequenceGenerator.swift
+//  BioSwiftGenerate
+//
+//  Created by Alexandre Lopoukhine on 27/03/2015.
+//  Copyright (c) 2015 bluetatami. All rights reserved.
+//
+
+import Foundation
+
+public class DNASequenceGenerator {
+    
+}

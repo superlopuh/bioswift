23e14ae51ea71d691905e08dcd902e63749e5b58
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-03-27 15:44:30 +0000

    generate ProbDNASequences that could be extracted from FASTQ
    FASTQ uses discrete score values, which limits possible probabilities
    to about 40 values.
    It also allows for an unknown value.

--- BioSwiftGenerate/BioSwiftGenerate/ProbDNASequenceGenerator.swift
+++ BioSwiftGenerate/BioSwiftGenerate/ProbDNASequenceGenerator.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import BioSwiftCore
+import BioSwiftFastq
 
 public class ProbDNASequenceGenerator {
     public static func generateSequence(ofLength length: Int) -> ProbDNASequence {
@@ -33,6 +34,43 @@ public class ProbDNASequenceGenerator {
             let errorProb = generateErrorProb()
             
             probNucleotideArray.append(ProbNucleotide.Known(nucleotide, errorProb))
+        }
+        
+        assert(probNucleotideArray.count == length, "NucleotideArray length doesn't match up")
+        
+        return ProbDNASequence(probNucleotideArray)
+    }
+    
+    // FASTQ sequences
+    public static func generateSequenceLikeFASTQ(ofLength length: Int, fastqType: FASTQType) -> ProbDNASequence {
+        var probNucleotideArray = [ProbNucleotide]()
+        
+        for i in 0..<length {
+            let randomNumber = arc4random_uniform(4)
+            
+            let nucleotide: Nucleotide
+            switch randomNumber {
+            case 0:
+                nucleotide = .A
+            case 1:
+                nucleotide = .C
+            case 2:
+                nucleotide = .G
+            case 3:
+                nucleotide = .T
+            default:
+                assertionFailure("Random number generator generated number out of 0..<4 range")
+            }
+            
+            let errorProb = generateErrorProb()
+            
+            let score = fastqType.probToScore(errorProb)
+            
+            if 0 == score {
+                probNucleotideArray.append(.Unknown)
+            } else {
+                probNucleotideArray.append(.Known(nucleotide, fastqType.scoreToProb(score)))
+            }
         }
         
         assert(probNucleotideArray.count == length, "NucleotideArray length doesn't match up")

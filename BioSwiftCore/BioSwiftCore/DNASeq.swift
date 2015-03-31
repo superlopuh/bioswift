5a7eec82acd1938e95958a1d891d8e5bfc9971cd
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-03-31 17:29:48 +0100

    DNASeq has a few of the other DNASequences' methods

--- BioSwiftCore/BioSwiftCore/DNASeq.swift
+++ BioSwiftCore/BioSwiftCore/DNASeq.swift
@@ -9,4 +9,32 @@
 import Foundation
 
 public struct DNASeq<N: NucleotideType> {
+    public var nucleotideArray: [N]
+    public var length: Int {
+        return nucleotideArray.count
+    }
+    
+    public init<S: SequenceType where S.Generator.Element == N>(_ nucleotideSequence: S) {
+        var nucleotideArray: [N] = []
+        
+        for nucleotide in nucleotideSequence {
+            nucleotideArray.append(nucleotide)
+        }
+        
+        self.nucleotideArray = nucleotideArray
+    }
+    
+    public func subsequence(subRange: Range<Int>) -> DNASeq<N> {
+        assert(subRange.startIndex >= 0 && subRange.endIndex <= length, "Subrange invalid")
+        
+        return DNASeq<N>(nucleotideArray[subRange])
+    }
+    
+    public subscript (index: Int) -> N {
+        return nucleotideArray[index]
+    }
+    
+    public subscript (subRange: Range<Int>) -> DNASeq<N> {
+        return subsequence(subRange)
+    }
 }

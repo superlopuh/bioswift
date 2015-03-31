eb26724c03485aab872b49a68e8d373880598bd1
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-03-31 17:13:00 +0100

    created struct DNASeq in core

---
+++ BioSwiftCore/BioSwiftCore/DNASeq.swift
@@ -0,0 +1,12 @@
+//
+//  DNASeq.swift
+//  BioSwiftCore
+//
+//  Created by Alexandre Lopoukhine on 31/03/2015.
+//  Copyright (c) 2015 bluetatami. All rights reserved.
+//
+
+import Foundation
+
+public struct DNASeq<N: NucleotideType> {
+}

64f75ddd357d6c2e3c40e1a44d263f0f28992cd9
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-04-18 15:30:15 +0100

    use new FaSTQStringsSequence in getArrayFromFile (filtered)

--- BioSwiftFastq/BioSwiftFastq/FASTQReader.swift
+++ BioSwiftFastq/BioSwiftFastq/FASTQReader.swift
@@ -32,8 +32,6 @@ public class FASTQReader {
             println("FASTQ file length \(fileContent.length)")
             let fastqStringArray = fileContent.componentsSeparatedByCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()) as! [String]
             
-            let hello = lazy(fastqStringArray)
-            
             let dateStart = NSDate()
             
             println("Date start: \(dateStart)")
@@ -60,39 +58,42 @@ public class FASTQReader {
     
     public static func getArrayFromFile(fileAddress: NSURL, ofFASTQType fastqType: FASTQType, @noescape usingFilter filter: FASTQ -> Bool) -> [FASTQ] {
         
+        let fastqStringsSequence = FASTQReader.getFASTQStringsSequenceFromFile(fileAddress, ofFASTQType: fastqType)
+        
+        let numberOfFASTQs = fastqStringsSequence.count
+        var fastqRead = 0
+        var printed = false
+        
         var sequences = [FASTQ]()
         
-        if let fileContent = NSString(contentsOfURL: fileAddress, encoding: NSUTF8StringEncoding, error: nil) {
-            println("FASTQ file length \(fileContent.length)")
-            let fastqStringArray = fileContent.componentsSeparatedByCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()) as! [String]
-            
-            let numberOfLines = fastqStringArray.count
-            var linesRead = 0
-            var printed = false
-            
-            let dateStart = NSDate()
-            
-            println("Date start: \(dateStart)")
-            
-            var gen = fastqStringArray.generate()
-            while
-                let infoString    = gen.next(),
-                let dnaString     = gen.next(),
-                let plusString    = gen.next(),
-                let qualityString = gen.next(){
-                    linesRead += 4
-                    if linesRead > 100000 && !printed {
-                        println("100000 lines parsed")
-                        println(NSDate().timeIntervalSinceDate(dateStart))
-                        printed = true
-                    }
-                    if let fastq = FASTQ(fastqInfoString: infoString, dnaString: dnaString, qualityString: qualityString, fastqType: fastqType) {
-                        if filter(fastq) {
-                            sequences.append(fastq)
-                        }
-                    } else {
-                        break
-                    }
+        let dateStart = NSDate()
+        
+        println("Date start: \(dateStart)")
+        
+        for fastqStrings in fastqStringsSequence {
+            fastqRead++
+            if fastqRead > 100000 && !printed {
+                println("100000 fastq parsed")
+                println(NSDate())
+                
+                let secondsElapsed = NSDate().timeIntervalSinceDate(dateStart)
+                
+                println("Seconds since start: \(secondsElapsed)")
+                
+                let secondsLeft = secondsElapsed * Double(numberOfFASTQs - 100000)/100000.0
+                
+                println("Seconds left: \(secondsLeft)")
+                
+                println("Finish estimate: \(NSDate().dateByAddingTimeInterval(secondsLeft))")
+                
+                printed = true
+            }
+            if let fastq = FASTQ(fastqInfoString: fastqStrings.fastqInfoString, dnaString: fastqStrings.dnaString, qualityString: fastqStrings.qualityString, fastqType: fastqType) {
+                if filter(fastq) {
+                    sequences.append(fastq)
+                }
+            } else {
+                break
             }
         }
         // End of file

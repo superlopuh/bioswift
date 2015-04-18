ec3bbace0f60031657590efd3f6bbea8e6169722
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-04-18 12:33:53 +0100

    added FASTQStrings struct

---
+++ BioSwiftFastq/BioSwiftFastq/FASTQStrings.swift
@@ -0,0 +1,16 @@
+//
+//  FASTQStrings.swift
+//  BioSwiftFastq
+//
+//  Created by Alexandre Lopoukhine on 18/04/2015.
+//  Copyright (c) 2015 bluetatami. All rights reserved.
+//
+
+import Foundation
+
+public struct FASTQStrings {
+    // Original Info
+    public let fastqInfoString: String
+    public let dnaString: String
+    public let qualityString: String
+}

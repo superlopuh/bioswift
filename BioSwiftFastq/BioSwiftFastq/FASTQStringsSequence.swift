272c8fb83ad1256973ca153e64e6c66885988671
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-04-18 15:04:21 +0100

    created new FASTQStringsSequence to replace lazy
    LazyFASTQStringsCollection was neither lazy nor a collection

---
+++ BioSwiftFastq/BioSwiftFastq/FASTQStringsSequence.swift
@@ -0,0 +1,33 @@
+//
+//  FASTQStringsSequence.swift
+//  BioSwiftFastq
+//
+//  Created by Alexandre Lopoukhine on 18/04/2015.
+//  Copyright (c) 2015 bluetatami. All rights reserved.
+//
+
+import Foundation
+
+struct FASTQStringsSequence<S: SequenceType where S.Generator.Element == String>: SequenceType {
+    private var stringsSequence: S
+    
+    internal init(stringsSequence: S) {
+        self.stringsSequence = stringsSequence
+    }
+    
+    func generate() -> GeneratorOf<FASTQStrings> {
+        var gen = stringsSequence.generate()
+        
+        return GeneratorOf<FASTQStrings> {
+            if
+                let infoString    = gen.next(),
+                let dnaString     = gen.next(),
+                let plusString    = gen.next(),
+                let qualityString = gen.next(){
+                    return FASTQStrings(fastqInfoString: infoString, dnaString: dnaString, qualityString: qualityString)
+            } else {
+                return nil
+            }
+        }
+    }
+}

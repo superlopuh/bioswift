eb93cde09629835612366460785909651b175854
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-04-19 16:09:54 +0100

    added constants to FASTQDistribution

--- BioSwiftFastq/BioSwiftFastq/FASTQDistribution.swift
+++ BioSwiftFastq/BioSwiftFastq/FASTQDistribution.swift
@@ -8,6 +8,18 @@
 
 import Foundation
 
-class FASTQDistribution {
+public class FASTQDistribution {
+    public let aDistribution: [Character:Int]
+    public let tDistribution: [Character:Int]
+    public let gDistribution: [Character:Int]
+    public let cDistribution: [Character:Int]
+    public let nDistribution: [Character:Int]
     
+    public init(aDistribution: [Character:Int], tDistribution: [Character:Int], gDistribution: [Character:Int], cDistribution: [Character:Int], nDistribution: [Character:Int]) {
+        self.aDistribution = aDistribution
+        self.tDistribution = tDistribution
+        self.gDistribution = gDistribution
+        self.cDistribution = cDistribution
+        self.nDistribution = nDistribution
+    }
 }

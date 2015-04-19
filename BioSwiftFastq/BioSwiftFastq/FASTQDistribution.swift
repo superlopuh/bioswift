b57b4126b17fdbb790f316e0d006d2fb6f02c74a
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-04-19 16:01:45 +0100

    created FASTQDistribution class

---
+++ BioSwiftFastq/BioSwiftFastq/FASTQDistribution.swift
@@ -0,0 +1,13 @@
+//
+//  FASTQDistribution.swift
+//  BioSwiftFastq
+//
+//  Created by Alexandre Lopoukhine on 19/04/2015.
+//  Copyright (c) 2015 bluetatami. All rights reserved.
+//
+
+import Foundation
+
+class FASTQDistribution {
+    
+}

0d137d05561b4f1a2ef9aa384bf97c8d0abe1309
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-05-01 11:45:55 +0100

    added more generator functions to ProbDNASequenceGenerator

--- BioSwiftGenerate/BioSwiftGenerate/ProbDNASequenceGenerator.swift
+++ BioSwiftGenerate/BioSwiftGenerate/ProbDNASequenceGenerator.swift
@@ -11,14 +11,40 @@ import BioSwiftCore
 import BioSwiftFastq
 
 public class ProbDNASequenceGenerator {
-    public static func generateSequence(ofLength length: Int, @noescape errorProbGenerator: () -> ProbNucleotide) -> DNASeq<ProbNucleotide> {
+    public static func generateSequence(ofLength length: Int, @noescape probNucGenerator: () -> ProbNucleotide) -> DNASeq<ProbNucleotide> {
         var probNucleotideArray = [ProbNucleotide]()
         
         for i in 0..<length {
-            probNucleotideArray.append(errorProbGenerator())
+            probNucleotideArray.append(probNucGenerator())
         }
         
         assert(probNucleotideArray.count == length, "NucleotideArray length doesn't match up")
+        
+        return DNASeq<ProbNucleotide>(probNucleotideArray)
+    }
+    
+    public static func generateSequence(ofLength length: Int, @noescape probNucGenerator: Nucleotide -> ProbNucleotide) -> DNASeq<ProbNucleotide> {
+        let nucleotideArray = DNASequenceGenerator.generateSequence(ofLength: length).nucleotideArray
+        
+        var probNucleotideArray = [ProbNucleotide]()
+        
+        for i in 0..<length {
+            probNucleotideArray.append(probNucGenerator(nucleotideArray[i]))
+        }
+        
+        assert(probNucleotideArray.count == length, "NucleotideArray length doesn't match up")
+        
+        return DNASeq<ProbNucleotide>(probNucleotideArray)
+    }
+    
+    public static func generateSequence(fromDNASequence dnaSequence: DNASeq<Nucleotide>, @noescape probNucGenerator: Nucleotide -> ProbNucleotide) -> DNASeq<ProbNucleotide> {
+        let nucleotideArray = dnaSequence.nucleotideArray
+        
+        var probNucleotideArray = [ProbNucleotide]()
+        
+        for i in 0..<dnaSequence.length {
+            probNucleotideArray.append(probNucGenerator(nucleotideArray[i]))
+        }
         
         return DNASeq<ProbNucleotide>(probNucleotideArray)
     }
